cbe6f07da5a01e4e8e12b1d98c72379938936840
FERNANDO DE LA TORRE <96254499+FernandoDLT@users.noreply.github.com>
2024-03-27 18:08:44 -0600

    Added form-submit functionality

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ window.addEventListener('load', function () {
         }
 
         dismissButton.addEventListener("click", (e) => {
-            window.location.href = "index.html"
+            card_1.classList.add('hide'); // Hide the success message
             e.preventDefault();
         });
     } else {
@@ -51,26 +51,161 @@ window.addEventListener('load', function () {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const navBar = document.querySelector('.nav');
-    let lastScrollTop = 0;
 
-    if (navBar) {
-        window.addEventListener('scroll', () => {
-            const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// window.addEventListener('load', function () {
+//     const form = document.querySelector('form');
+//     const box = document.querySelector('.box');
+//     const emailInput = document.getElementById('email');
+//     const subjectInput = document.getElementById('subject');
+//     const messageInput = document.getElementById('message');
+//     const card_1 = document.querySelector(".card");
+//     const dismissButton = document.querySelector(".dismiss");
 
-            if (currentScrollTop <= 0) {
-                // At the top of the page, hide the navigation menu
-                navBar.style.top = '-100%';
-            } else {
-                // Scrolling down or up, show the navigation menu
-                navBar.style.top = '0';
-            }
+//     if (form) {
+//         emailInput.addEventListener('input', function () {
+//             updateInputClass(emailInput);
+//         });
 
-            lastScrollTop = currentScrollTop;
-        });
-    }
-});
+//         form.addEventListener('submit', function (event) {
+//             event.preventDefault();
+
+//             // Gather form data
+//             const formData = new FormData(form);
+//             const email = formData.get('email');
+//             const subject = formData.get('subject');
+//             const message = formData.get('message');
+
+//             // Send form data to backend endpoint
+//             fetch('/sendEmail', {
+//                 method: 'POST',
+//                 body: JSON.stringify({ email, subject, message }),
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 }
+//             })
+//             .then(response => response.text())
+//             .then(message => {
+//                 console.log(message); // Output server response
+//                 // Show success message
+//                 card_1.classList.remove('hide');
+//                 // Reset form
+//                 form.reset();
+//             })
+//             .catch(error => console.error('Error:', error));
+//         });
+
+//         function updateInputClass(inputElement) {
+//             if (inputElement.value.trim() !== '') {
+//                 inputElement.classList.add('has-value');
+//             } else {
+//                 inputElement.classList.remove('has-value');
+//             }
+//         }
+
+//         dismissButton.addEventListener("click", (e) => {
+//             window.location.href = "index.html"
+//             e.preventDefault();
+//         });
+//     } else {
+//         console.error('Form not found');
+//     }
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const navBar = document.querySelector('.nav');
+//     let lastScrollTop = 0;
+
+//     if (navBar) {
+//         window.addEventListener('scroll', () => {
+//             const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+//             if (currentScrollTop <= 0) {
+//                 // At the top of the page, hide the navigation menu
+//                 navBar.style.top = '-100%';
+//             } else {
+//                 // Scrolling down or up, show the navigation menu
+//                 navBar.style.top = '0';
+//             }
+
+//             lastScrollTop = currentScrollTop;
+//         });
+//     }
+// });
+
+
+// window.addEventListener('load', function () {
+//     const form = document.querySelector('form');
+//     const box = document.querySelector('.box');
+//     const emailInput = document.getElementById('email');
+//     const subjectInput = document.getElementById('subject');
+//     const messageInput = document.getElementById('message');
+//     const card_1 = document.querySelector(".card");
+//     const dismissButton = document.querySelector(".dismiss");
+
+//     if (form) {
+//         emailInput.addEventListener('input', function () {
+//             updateInputClass(emailInput);
+//         });
+
+//         form.addEventListener('submit', function (event) {
+//             event.preventDefault();
+
+//             // Gather form data
+//             const formData = new FormData(form);
+
+//             // Send form data to backend endpoint
+//             fetch('/sendEmail', {
+//                 method: 'POST',
+//                 body: formData
+//             })
+//             .then(response => response.text())
+//             .then(message => {
+//                 console.log(message); // Output server response
+//                 // Show success message
+//                 card_1.classList.remove('hide');
+//                 // Reset form
+//                 form.reset();
+//             })
+//             .catch(error => console.error('Error:', error));
+//         });
+
+//         function updateInputClass(inputElement) {
+//             if (inputElement.value.trim() !== '') {
+//                 inputElement.classList.add('has-value');
+//             } else {
+//                 inputElement.classList.remove('has-value');
+//             }
+//         }
+
+//         dismissButton.addEventListener("click", (e) => {
+//             window.location.href = "index.html"
+//             e.preventDefault();
+//         });
+//     } else {
+//         console.error('Form not found');
+//     }
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const navBar = document.querySelector('.nav');
+//     let lastScrollTop = 0;
+
+//     if (navBar) {
+//         window.addEventListener('scroll', () => {
+//             const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+//             if (currentScrollTop <= 0) {
+//                 // At the top of the page, hide the navigation menu
+//                 navBar.style.top = '-100%';
+//             } else {
+//                 // Scrolling down or up, show the navigation menu
+//                 navBar.style.top = '0';
+//             }
+
+//             lastScrollTop = currentScrollTop;
+//         });
+//     }
+// });
 
 
 // window.addEventListener('load', function () {
